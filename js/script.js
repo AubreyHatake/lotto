3bee7017b1727ae9f9df07f49db50a79ab0fc9e8
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-11-06 19:26:29 -0700

    style

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Variable
+var userNumber = prompt("How many numbers would you like?");
+// var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+document.getElementById("results").innerHTML =
+Math.floor(Math.random() * 99);
