ce6ef5d9eed151d1915429281d6d0ac31730b712
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-11-08 23:06:10 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 // Variable
 var userNumber = prompt("How many numbers would you like?");
-// var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numArray = [];
 
-document.getElementById("results").innerHTML =
-Math.floor(Math.random() * 99);
+for (let i = 0; i < userNumber; i++)
+{
+    numArray[i] = Math.floor(Math.random() * 99);
+}
+
+document.getElementById("results").innerHTML = numArray
