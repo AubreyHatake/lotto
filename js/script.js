d19e99f19820d31c2943cea0cd11a4c60d222e55
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-11-09 00:14:09 -0700

    i did ittt

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
-// Variable
+// Variables
 var userNumber = prompt("How many numbers would you like?");
 var numArray = [];
+var lottoPicks = "";
 
+
+// Fills the array
 for (let i = 0; i < userNumber; i++)
 {
     numArray[i] = Math.floor(Math.random() * 99);
 }
 
-document.getElementById("results").innerHTML = numArray
+
+function displayNumbers() {
+// Displays array to the page 
+for (var i = 0; i < numArray.length; i++)
+if (i == userNumber-1)
+{
+    lottoPicks += numArray[i]
+    document.getElementById("results").innerHTML = lottoPicks;
+}
+else
+{ 
+    lottoPicks += numArray[i] + "-";
+ document.getElementById("results").innerHTML = lottoPicks;
+}
+}
+// if the user enters a number less than 9 it will display their numbers, if not it will display a string.
+if (userNumber < 9)
+{
+    displayNumbers();
+}
+else
+{
+    document.getElementById("results").innerHTML = "Enter a number less than 9"
+}
+
+document.getElementById("btn").addEventListener("click", displayNumbers);
